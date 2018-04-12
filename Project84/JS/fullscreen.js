0cf342ed4db2bd893cb0c62539036cf4ec8d3702
Adam <mr_adam_man@hotmail.com>
2018-04-12 13:20:31 +0100

    Update fullscreen.js

--- Project84/JS/fullscreen.js
+++ Project84/JS/fullscreen.js
@@ -1,1 +1,24 @@
 
+$(document).ready(function(){
+
+    if (
+	document.fullscreenEnabled || 
+	document.webkitFullscreenEnabled || 
+	document.mozFullScreenEnabled ||
+	document.msFullscreenEnabled
+) {
+    var i = document.querySelector("body");
+
+// go full-screen
+if (i.requestFullscreen) {
+	i.requestFullscreen();
+} else if (i.webkitRequestFullscreen) {
+	i.webkitRequestFullscreen();
+} else if (i.mozRequestFullScreen) {
+	i.mozRequestFullScreen();
+} else if (i.msRequestFullscreen) {
+	i.msRequestFullscreen();
+}
+}
+    
+});
